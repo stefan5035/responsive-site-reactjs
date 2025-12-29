13b5e2a2defc6a0886fb4d37f97df3b9cb7d74d1
stefan5035 <stefan.madethen@gmail.com>
2025-12-29 15:56:15 +0700

    livecode 002

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from './components/nav'
-
+import Hero from './components/hero'
+import Client from './components/client'
 export default function App() {
   return (
     <div className="app">
       <Navbar />
+      <Hero />
+      <Client />
    </div>
   )
 }
---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="container hero-grid">
+        <div>
+          <h1>Lessons and insights<br /><span>from 8 years</span></h1>
+          <p>Where to grow your business as a photographer: site or social media?</p>
+          <button className="btn primary">Register</button>
+        </div>
+        <div className="hero-img"><img src="src/assets/hero_image.png"/></div>
+      </div>
+    </section>
+  )
+}
