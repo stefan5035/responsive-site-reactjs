b2b1dc47f527a8c9e60ca21e8f148318d71b84d6
stefan5035 <stefan.madethen@gmail.com>
2025-12-30 09:55:13 +0700

    livecode 006

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/nav'
 import Hero from './components/hero'
 import Client from './components/client'
 import Community from './components/comm'
+import Feature from './components/ftr'
 export default function App() {
   return (
     <div className="app">
@@ -9,6 +10,7 @@ export default function App() {
       <Hero />
       <Client />
       <Community />
+      <Feature />
    </div>
   )
 }
---
+++ src/components/ftr.jsx
@@ -0,0 +1,14 @@
+export default function Feature() {
+  return (
+    <section className="feature">
+      <div className="container feature-grid">
+        <div className="feature-img"><img src="src/assets/feature_image.png"/></div>
+        <div>
+          <h2>The unseen of spending three years at Pixelgrade</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button className="btn primary">Learn More</button>
+        </div>
+      </div>
+    </section>
+  )
+}
