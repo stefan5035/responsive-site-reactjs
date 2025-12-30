45886afcb36ecc69f3c0aba869e7b480f9474460
stefan5035 <stefan.madethen@gmail.com>
2025-12-30 12:13:46 +0700

    livecode 008

--- src/components/stat.jsx
+++ src/components/stat.jsx
@@ -1,27 +1,64 @@
+import stats1 from '../assets/stats1.png'
+import stats2 from '../assets/stats2.png'
+import stats3 from '../assets/stats3.png'
+import stats4 from '../assets/stats4.png'
+
+const stats = [
+  {
+    icon: stats1,
+    value: '2,245,341',
+    label: 'Members',
+  },
+  {
+    icon: stats2,
+    value: '46,328',
+    label: 'Clubs',
+  },
+  {
+    icon: stats3,
+    value: '828,867',
+    label: 'Event Bookings',
+  },
+  {
+    icon: stats4,
+    value: '1,926,436',
+    label: 'Payments',
+  },
+]
+
 export default function Stats() {
   return (
     <section className="stats">
       <div className="container stats-grid">
         <div>
-          <h2>Helping a local<br /><span>business reinvent itself</span></h2>
+          <h2>
+            Helping a local <br />
+            <span>business reinvent itself</span>
+          </h2>
           <p>We reached here with our hard work and dedication</p>
         </div>
+
         <div className="numbers">
-          <Stt label="Members" value="2,245,341" />
-          <Stt label="Clubs" value="46,328" />
-          <Stt label="Event Bookings" value="828,867" />
-          <Stt label="Payments" value="1,926,436" />
+          {stats.map((stat) => (
+            <Stt key={stat.label} {...stat} />
+          ))}
         </div>
       </div>
     </section>
   )
 }
-function Stt({ label, value }) {
+
+function Stt({ icon, value, label }) {
   return (
     <div className="stat">
-      <strong>{value}</strong>
-      <br/>
-      <span>{label}</span>
+      <div className="stat-icon">
+        <img src={icon} alt="" />
+      </div>
+      <div>
+        <strong>{value}</strong>
+        <br/>
+        <span>{label}</span>
+      </div>
     </div>
   )
 }
